e58219db13728d8f6318172984f13d19b6f4e4cc
Hayoung Kang <rkdgkdudsam@naver.com>
2025-06-27 11:22:23 +0900

    feat: Main 페이지 퍼블리싱

---
+++ src/pages/Main.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+export default function Main() {
+    const flameImages = ["/flame1.png", "/flame2.png"];
+    const waveImages = ["/wave1.png", "/wave2.png", "/wave3.png", "/wave4.png"];
+    const [flameIndex, setFlameIndex] = useState(0);
+    const [waveIndex, setWaveIndex] = useState(0);
+
+    useEffect(() => {
+        const flameInterval = setInterval(() => {
+            setFlameIndex((prev: number) => (prev + 1) % flameImages.length);
+        }, 500);
+
+        return () => clearInterval(flameInterval);
+    }, [flameImages.length]);
+
+    useEffect(() => {
+        const waveInterval = setInterval(() => {
+            setWaveIndex((prev: number) => (prev + 1) % waveImages.length);
+        }, 1000);
+
+        return () => clearInterval(waveInterval);
+    }, [waveImages.length]);
+
+    return (
+        <>
+            <div className="relative h-screen w-screen">
+                {/* 배경 */}
+                <div className="absolute inset-0 bg-[linear-gradient(to_bottom,_#212453_0%,_#432E5B_40%,_#723D59_60%,_#C54C52_70%)]" />
+
+                <div className="bg-img bg-[url('/sky.png')]" />
+                <div className="absolute bottom-0 w-full h-[20%] bg-[#836161]" />
+                <div
+                    className={`bg-img bg-[url('${waveImages[waveIndex]}')]`}
+                />
+                <div className="bg-img bg-[url('/clouds.png')] animate-cloud" />
+                <div className="bg-img bg-[url('/character1.png')]" />
+                <div
+                    className={`bg-img bg-[url('${flameImages[flameIndex]}')]`}
+                />
+
+                {/* 텍스처 필터 */}
+                <div className="absolute inset-0 bg-[url('/filter.svg')] bg-repeat opacity-30 mix-blend-multiply" />
+
+                {/* 텍스트 */}
+                <div className="relative pt-[9%] flex flex-col items-center h-full text-center text-white">
+                    <h1 className="stardust text-8xl cursor-default">
+                        사용자를 생각하는 프론트엔드
+                        <br />
+                        <span className="neodunggeunmo">강하영</span>입니다.
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+}
